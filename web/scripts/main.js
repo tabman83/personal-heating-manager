09b04ef8b2f98fa099b702ffbffffd512b48c026
Antonino Parisi <tabman83@gmail.com>
2015-03-02 16:43:10 +0000

    Disabled overscroll.

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -22,7 +22,7 @@
             });
 
             //$locationProvider.html5Mode(true);
-        }]).run(['$rootScope', '$window', function($rootScope, $window) {
+        }]).run(['$rootScope', '$window', '$document', function($rootScope, $window, $document) {
 
             // Hook success
             $rootScope.$on('$routeChangeSuccess', function() {
@@ -30,6 +30,67 @@
                 $window.scrollTo(0, 0);
             });
 
+            // overscroll fix
+            var scrollable = angular.element('.scrollable').get(0);
+            var startY, startTopScroll;
+            scrollable.addEventListener('touchstart', function(event) {
+                startY = event.touches[0].pageY;
+        		startTopScroll = scrollable.scrollTop;
+        		if(startTopScroll <= 0) {
+                    scrollable.scrollTop = 1;
+                }
+        		if(startTopScroll + scrollable.offsetHeight >= scrollable.scrollHeight) {
+                    scrollable.scrollTop = scrollable.scrollHeight - scrollable.offsetHeight - 1;
+                }
+            }, false);
+
+            $document.get(0).addEventListener('touchmove', function(event) {
+                var fixedHeader = angular.element(event.target).closest('.navbar-fixed-top');
+                if(fixedHeader.length > 0) {
+                   event.preventDefault();
+               }
+           }, false);
+
         }]);
 
 })(angular);
+
+/* jshint ignore:start */
+/**
+ * ScrollFix v0.1
+ * http://www.joelambert.co.uk
+ *
+ * Copyright 2011, Joe Lambert.
+ * Free to use under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+var ScrollFix = function(elem) {
+	// Variables to track inputs
+	var startY, startTopScroll;
+
+	elem = elem || document.querySelector(elem);
+
+	// If there is no element, then do nothing
+	if(!elem)
+		return;
+
+	// Handle the start of interactions
+	elem.addEventListener('touchstart', function(event){
+		startY = event.touches[0].pageY;
+		startTopScroll = elem.scrollTop;
+
+		if(startTopScroll <= 0)
+			elem.scrollTop = 1;
+
+		if(startTopScroll + elem.offsetHeight >= elem.scrollHeight)
+			elem.scrollTop = elem.scrollHeight - elem.offsetHeight - 1;
+	}, false);
+};
+
+$(function() {
+    //new ScrollFix($('div.scrollable').get(0));
+})
+
+
+/* jshint ignore:end */
