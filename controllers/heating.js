2cc18b7e4ac9263096e6f8c549e947d584e86ae7
Antonino Parisi <tabman83@gmail.com>
2015-03-15 19:13:58 +0000

    Reworked aggregation reports.

--- controllers/heating.js
+++ controllers/heating.js
@@ -1,42 +1,5 @@
 var Heating = require('mongoose').model('Heating');
 
-var noAggregationProjectionDate = 1;
-
-var dailyAggregationProjectionDate = {
-	$subtract: [ '$date', {
-		$add: [ '$ml', { $multiply: [ '$s', 1000 ] }, { $multiply: [ '$m', 60, 1000 ] }, { $multiply: [ '$h', 60, 60, 1000 ] } ]
-	}]
-}
-
-var monthlyAggregationProjectionDate = {
-	$subtract: [ '$date', {
-		$add: [ '$ml', { $multiply: [ '$s', 1000 ] }, { $multiply: [ '$m', 60, 1000 ] }, { $multiply: [ '$h', 60, 60, 1000 ] }, { $multiply: [ { $subtract: ['$d', 1] }, 24, 60, 60, 1000 ] } ]
-	}]
-}
-
-var dailyAggregationProjection = {
-	date: 1,
-	h: { $hour: '$date' },
-	m: { $minute: '$date' },
-	s: { $second: '$date' },
-	ml: { $millisecond: '$date' },
-	timestamp: { $cond: [ '$value', { $multiply: [-1, { $subtract: [ '$date', new Date('1970-01-01') ] }] }, { $multiply: [+1, { $subtract: [ '$date', new Date('1970-01-01') ] }] } ] }
-};
-
-var monthlyAggregationProjection = {
-	date: 1,
-	d: { $dayOfMonth: '$date' },
-	h: { $hour: '$date' },
-	m: { $minute: '$date' },
-	s: { $second: '$date' },
-	ml: { $millisecond: '$date' },
-	timestamp: { $cond: [ '$value', { $multiply: [-1, { $subtract: [ '$date', new Date('1970-01-01') ] }] }, { $multiply: [+1, { $subtract: [ '$date', new Date('1970-01-01') ] }] } ] }
-};
-
-var noAggregationProjection = {
-	timestamp: { $cond: [ '$value', { $multiply: [-1, { $subtract: [ '$date', new Date('1970-01-01') ] }] }, { $multiply: [+1, { $subtract: [ '$date', new Date('1970-01-01') ] }] } ] }
-};
-
 function HeatingStatusController() { }
 
 HeatingStatusController.prototype = {
@@ -82,6 +45,24 @@ HeatingStatusController.prototype = {
 		var begin = request.query.begin ? new Date(request.query.begin) : new Date('2000-01-01');
 		var end = request.query.end ? new Date(request.query.end) : new Date('2100-01-01');
 
+
+		var grouping = {};
+		switch(aggregation) {
+			case 'day':
+				grouping = { dayOfYear: '$_id.dayOfYear', year: '$_id.year' };
+				break;
+			case 'month':
+				grouping = { month: '$_id.month', year: '$_id.year' };
+				break;
+			case 'week':
+				grouping = { dayOfWeek: '$_id.week', year: '$_id.year' };
+				break;
+			default:
+				grouping = null;
+				aggregation = 'none';
+		}
+
+
 		var stageMatch = {
 			$match: {
 				switchedOn: {
@@ -95,6 +76,7 @@ HeatingStatusController.prototype = {
 		        switchedOn: 1,
 		        switchedOff: 1,
 		        duration: 1,
+				week: { $week: '$switchedOn' },
 		        dayOfYear: { $dayOfYear: '$switchedOn' },
 		        month: { $month: '$switchedOn' },
 		        year: { $year: '$switchedOn' },
@@ -104,6 +86,7 @@ HeatingStatusController.prototype = {
 			$group : {
 		        _id : {
 		            dayOfYear: '$dayOfYear',
+					week: '$week',
 		            month: '$month',
 		            year: '$year',
 		            duration: '$duration',
@@ -112,30 +95,26 @@ HeatingStatusController.prototype = {
 		        }
 			}
 		}
+		var stageMidSort = {
+			$sort: { 'switchedOn': -1 }
+		}
 		var stagePush = {
 		    $group : {
-		        _id :  {
-		            dayOfYear: '$_id.dayOfYear',
-		            year: '$_id.year'
-		        },
+		        _id : grouping,
 		        dates: {
 		            $push: {
-		                switchedOff:'$_id.switchedOff',
-		                switchedOn:'$_id.switchedOn',
-		                duration:'$_id.duration'
+		                switchedOff: '$_id.switchedOff',
+		                switchedOn: '$_id.switchedOn',
+		                duration: '$_id.duration'
 		            }
 		        },
+				date: { $first: '$_id.switchedOn' },
 		        duration: { $sum : '$_id.duration' }
 		    }
 		};
-		var stageSort = {
-			$sort: {
-		        '_id.year': -1,
-		        '_id.dayOfYear': -1
-		    }
-		};
+		var stageSort = { $sort: { 'date': -1 } };
 
-		Heating.aggregate([stageMatch, stageProject, stageGrouping, stagePush, stageSort], queryCallback);
+		Heating.aggregate([stageMatch, stageProject, stageGrouping, stageMidSort, stagePush, stageSort], queryCallback);
 
 		function queryCallback(err, result) {
 			if (err) {
